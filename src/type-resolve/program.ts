7b667b37150d9de7cbe18d78f1e994e5431bd7ab
GoogleFeud <volen.sl666@gmail.com>
2023-08-22 00:23:09 +0300

    Show types correctly in editor

--- src/type-resolve/program.ts
+++ src/type-resolve/program.ts
@@ -1,15 +1,15 @@
 import * as ts from "typescript";
 import type { ProgramTransformerExtras, PluginConfig } from "ts-patch";
-import { MacroTransformer } from "./transformer";
-import { TsMacrosConfig, macros } from "./index";
+import { MacroTransformer } from "../transformer";
+import { TsMacrosConfig, macros } from "../index";
 
-export function printAsTS(printer: ts.Printer, source: ts.SourceFile) : string {
+export function printAsTS(printer: ts.Printer, statements: ts.NodeArray<ts.Statement>, source: ts.SourceFile) : string {
     let fileText = "";
-    for (const fileItem of source.statements) {
+    for (const fileItem of statements) {
         fileText += printer.printNode(ts.EmitHint.Unspecified, fileItem, source);
     }
     return fileText;
-} 
+}
 
 export function patchCompilerHost(host: ts.CompilerHost | undefined, config: ts.CompilerOptions | undefined, newSourceFiles: Map<string, ts.SourceFile>, instance: typeof ts) : ts.CompilerHost {
     const compilerHost = host || instance.createCompilerHost(config || instance.getDefaultCompilerOptions(), true);
@@ -29,10 +29,12 @@ export default function (
     options: PluginConfig, 
     extras: ProgramTransformerExtras
 ) : ts.Program {
+    const isTSC = process.argv[1]?.endsWith("tsc");
+
     const instance = extras.ts as typeof ts;
     const transformer = new MacroTransformer(instance.nullTransformationContext, program.getTypeChecker(), macros, options as TsMacrosConfig);
     const newSourceFiles = new Map();
-
+    const compilerOptions = program.getCompilerOptions();
     const printer = instance.createPrinter();
 
     for (const sourceFile of program.getSourceFiles()) {
@@ -40,15 +42,24 @@ export default function (
     
         const parsed = transformer.run(sourceFile);
         if (!instance.isSourceFile(parsed)) continue;
-        newSourceFiles.set(sourceFile.fileName, instance.createSourceFile(sourceFile.fileName, printAsTS(printer, parsed), sourceFile.languageVersion));
+        if (isTSC) newSourceFiles.set(sourceFile.fileName, instance.createSourceFile(sourceFile.fileName, printAsTS(printer, parsed.statements, parsed), sourceFile.languageVersion, true, ts.ScriptKind.TS));
+        else {
+            const newNodes = [];
+            for (const statement of parsed.statements) {
+                if (statement.pos === -1 && (ts.isTypeDeclaration(statement) || ts.isVariableStatement(statement))) {
+                    newNodes.push(statement);
+                }
+            }
+            const newNodesOnly = printAsTS(printer, instance.factory.createNodeArray(newNodes), parsed);
+            const newNodesSource = instance.createSourceFile(sourceFile.fileName, sourceFile.text + "\n" + newNodesOnly, sourceFile.languageVersion, true, ts.ScriptKind.TS);
+            ts.sys.writeFile(`${sourceFile.fileName}_log.txt`, newNodesSource.text);
+            newSourceFiles.set(sourceFile.fileName, newNodesSource); 
+        }
     }
-
-    const compilerOptions = program.getCompilerOptions();
 
     return instance.createProgram(
         program.getRootFileNames(),
         compilerOptions,
-        patchCompilerHost(host, compilerOptions, newSourceFiles, instance),
-        program
+        patchCompilerHost(host, compilerOptions, newSourceFiles, instance)
     );
 }
