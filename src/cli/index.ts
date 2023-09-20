2bcbe19a031ad67dc4c637087a6abd26b1307e34
GoogleFeud <volen.sl666@gmail.com>
2023-09-20 23:29:27 +0300

    Start work on CLI for usage with other typescript transpilers besides tsc

---
+++ src/cli/index.ts
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+import * as parseArgs from "yargs-parser";
+import * as ts from "typescript";
+import { PretranspileSettings, pretranspile, validateSettings } from "./pretranspile";
+import { cyan, emitError, emitNotification } from "./formatter";
+
+type CLIArgs = {
+    _: string[],
+} & Omit<PretranspileSettings, "dist">;
+
+(() => {
+
+    const args = parseArgs(process.argv.slice(2)) as CLIArgs;
+    const command = args._[0];
+
+    if (command === "transform") {
+        const dist = args._[1];
+        if (!dist || typeof dist !== "string") return emitError`Please provide an out folder path.\n\nUsage: ts-macros transform [PATH]`;
+        const validatedSettings = validateSettings(args);
+        if (validatedSettings.length) return emitError`Setting errors:\n${validatedSettings.join(", ")}`;
+        const errors = pretranspile({
+            dist,
+            ...args
+        });
+
+        if (errors) console.log(ts.formatDiagnosticsWithColorAndContext(errors, {
+            getNewLine: () => "\r\n",
+            getCurrentDirectory: () => "unknown directory",
+            getCanonicalFileName: (fileName) => fileName
+        }));
+    }
+    else if (command === "help") emitHelp();
+    else {
+        emitNotification`Unknown command ${command}.`;
+        emitHelp();
+    }
+})();
+
+
+function emitHelp() : void {
+    emitNotification`ts-macros CLI args
+
+Commands:
+* ts-macros transform [OUT] - Expand all macros and write transformed TS files to the selected OUT directory.
+    ${cyan("Example")}: transform ./transformed --noComptime
+    -- noComptime   - Disable usage of $$raw and $$comptime macros.
+    -- exec=[CMD]   - Execute a command after writing the transformed typescript files to disk.
+    -- cleanup      - Delete the OUT directory after executing CMD.
+    -- tsconfig     - Point the transformer to a different tsconfig.json file.
+    -- watch        - Transformer will transform your files on changes. Cleanup and exec options are ignored.
+`;
+}
---
+++ src/cli/pretranspile.ts
@@ -0,0 +1,71 @@
+import * as ts from "typescript";
+import * as path from "path";
+import * as childProcess from "child_process";
+import * as fs from "fs";
+import { MacroTransformer } from "../transformer";
+import { macros } from "..";
+
+export interface PretranspileSettings {
+    dist: string,
+    exec?: string,
+    tsconfig?: string,
+    cleanup?: boolean,
+    watch?: boolean,
+    noComptime?: boolean,
+}
+
+export function pretranspileFile(sourceFile: ts.SourceFile, printer: ts.Printer, transformer: MacroTransformer) : string {
+    const newSourceFile = transformer.run(sourceFile);
+    return printer.printFile(newSourceFile);
+}
+
+export function createFile(providedPath: string, content: string) : void {
+    const withoutFilename = providedPath.slice(0, providedPath.lastIndexOf(path.sep));
+    if (!fs.existsSync(withoutFilename)) fs.mkdirSync(withoutFilename, { recursive: true });
+    fs.writeFileSync(providedPath, content);
+}
+
+export function createAnonDiagnostic(message: string) : ts.Diagnostic {
+    return ts.createCompilerDiagnostic({
+        key: "Errror",
+        code: 8000,
+        message,
+        category: ts.DiagnosticCategory.Error
+    });
+}
+
+export function pretranspile(settings: PretranspileSettings) : ts.Diagnostic[] | undefined {
+    const config = settings.tsconfig || ts.findConfigFile(process.cwd(), ts.sys.fileExists, "tsconfig.json");
+    if (!config) return [createAnonDiagnostic( "Couldn't find tsconfig.json file.")];
+
+    const distPath = path.join(process.cwd(), settings.dist);
+    if (!fs.existsSync(distPath)) fs.mkdirSync(distPath, { recursive: true });
+
+    if (settings.watch) {
+        // TODO: Implement watch
+    } else {
+        const readConfig = ts.parseConfigFileWithSystem(config, ts.getDefaultCompilerOptions(), undefined, undefined, ts.sys, () => undefined);
+        if (!readConfig) return [createAnonDiagnostic("Couldn't read tsconfig.json file.")];
+        if (readConfig.errors.length) return readConfig.errors;
+        const program = ts.createProgram({
+            rootNames: readConfig.fileNames,
+            options: readConfig.options
+        });
+        const transformer = new MacroTransformer(ts.nullTransformationContext, program.getTypeChecker(), macros, { noComptime: settings.noComptime });
+        const printer = ts.createPrinter();
+        for (const file of program.getSourceFiles()) {
+            if (file.isDeclarationFile) continue;
+            createFile(path.join(process.cwd(), settings.dist, file.fileName.slice(process.cwd().length)), pretranspileFile(file, printer, transformer));
+        }
+
+        if (settings.exec) childProcess.execSync(settings.exec);
+        if (settings.cleanup) fs.rmSync(settings.dist, { recursive: true, force: true });
+    }
+}
+
+export function validateSettings(settings: Record<string, unknown>) : string[] {
+    const errors = [];
+    if (settings.exec && typeof settings.exec !== "string") errors.push("Expected exec to be a string");
+    if (settings.tsconfig && typeof settings.tsconfig !== "string") errors.push("Expected tsconfig to be a string");
+    return errors;
+}
