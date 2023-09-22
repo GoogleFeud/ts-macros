dd3a37e6fec2cb9ec5f66aa25b51ef55b46ae0dc
GoogleFeud <volen.sl666@gmail.com>
2023-09-22 14:38:14 +0300

    Start work on watch mode

--- src/cli/pretranspile.ts
+++ src/cli/pretranspile.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 import * as childProcess from "child_process";
 import * as fs from "fs";
 import { MacroTransformer } from "../transformer";
-import { macros } from "..";
+import { TsMacrosConfig, macros } from "..";
 
 export interface PretranspileSettings {
     dist: string,
@@ -41,8 +41,34 @@ export function pretranspile(settings: PretranspileSettings) : ts.Diagnostic[] |
     const distPath = path.join(process.cwd(), settings.dist);
     if (!fs.existsSync(distPath)) fs.mkdirSync(distPath, { recursive: true });
 
+    const transformerConfig: TsMacrosConfig = { noComptime: settings.noComptime, keepImports: true };
+    const printer = ts.createPrinter();
+
     if (settings.watch) {
-        // TODO: Implement watch
+        const host = ts.createWatchCompilerHost(config, { noEmit: true }, ts.sys, ts.createSemanticDiagnosticsBuilderProgram, undefined, undefined, undefined, undefined);
+        const oldWrite = host.writeFile;
+        host.writeFile = (path, data) => {
+            console.log("WRITING:", path);
+            return oldWrite?.(path, data);
+        };
+        const oldCreateProgram = host.createProgram;
+        const transformer = new MacroTransformer(ts.nullTransformationContext, (undefined as unknown as ts.TypeChecker), macros, transformerConfig);
+        host.createProgram = (rootNames, options, host, oldProgram) => {
+            const newProgram = oldCreateProgram(rootNames, options, host, oldProgram);
+            transformer.checker = newProgram.getProgram().getTypeChecker();
+            const sourceFiles = [];
+            for (const source of newProgram.getProgram().getSourceFiles()) {
+                if (source.isDeclarationFile) continue;
+                const oldSource = oldProgram?.getSourceFile(source.fileName);
+                if (!oldSource || oldSource.version !== source.version) {
+                    const transpiled = pretranspileFile(source, printer, transformer);
+                    sourceFiles.push(ts.createSourceFile(source.fileName, transpiled, ts.ScriptTarget.ESNext, true, ts.ScriptKind.TS));
+                    createFile(path.join(process.cwd(), settings.dist, source.fileName.slice(process.cwd().length)), transpiled);
+                }
+            }
+            return newProgram;
+        };
+        ts.createWatchProgram(host);
     } else {
         const readConfig = ts.parseConfigFileWithSystem(config, ts.getDefaultCompilerOptions(), undefined, undefined, ts.sys, () => undefined);
         if (!readConfig) return [createAnonDiagnostic("Couldn't read tsconfig.json file.")];
@@ -51,8 +77,7 @@ export function pretranspile(settings: PretranspileSettings) : ts.Diagnostic[] |
             rootNames: readConfig.fileNames,
             options: readConfig.options
         });
-        const transformer = new MacroTransformer(ts.nullTransformationContext, program.getTypeChecker(), macros, { noComptime: settings.noComptime });
-        const printer = ts.createPrinter();
+        const transformer = new MacroTransformer(ts.nullTransformationContext, program.getTypeChecker(), macros, transformerConfig);
         for (const file of program.getSourceFiles()) {
             if (file.isDeclarationFile) continue;
             createFile(path.join(process.cwd(), settings.dist, file.fileName.slice(process.cwd().length)), pretranspileFile(file, printer, transformer));
